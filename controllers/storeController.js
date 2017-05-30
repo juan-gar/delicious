86cfe83edf48da7035bd2faacbcacc91ace93d8c
Juan <juan.garcmin@gmail.com>
2017-05-31 00:10:38 +0200

    querying the db and rendering the results

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,7 +10,15 @@ exports.addStore = (req,res) => {
 	res.render('editStore', {title: 'Add Store'});
 };
 
-exports.createStore = (req,res) => {
-	const store = new Store(req.body);
-	store.save();
+exports.createStore = async (req,res) => {
+	const store = await (new Store(req.body)).save();
+	req.flash('success', `Successfully created ${store.name}. Leave a review?`)
+	res.redirect('/store/${store.slug}')
 };
+
+exports.getStores = async (req, res) => {
+//1. Query database for stores
+	const stores = await Store.find();
+	console.log(stores);
+	res.render('stores', { title: 'Stores', stores })
+}
